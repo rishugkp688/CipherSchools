30a30d6a7e34aac8449990eb81bc4cde174ca6be
Rishabh Singh <38781878+rishugkp688@users.noreply.github.com>
2024-07-06 20:05:20 +0530

    Add files via upload

---
+++ Lecture_16/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  const name='Rishabh'
+  return (
+
+    <>
+    <h1 style={{color:"blue",textAlign: "center"}}>WELCOME TO CIPHER SCHOOL</h1>
+    <div>
+    <p>Hello {name}!</p>
+    </div>
+    </>
+
+  );
+}
+
+export default App;
